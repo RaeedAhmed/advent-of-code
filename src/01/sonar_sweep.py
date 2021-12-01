e0fd43e17234a8c067f8c36a730a88cce6bb1d59
RaeedAhmed <ruahmed7868@gmail.com>
2021-12-01 12:01:08 -0800

    Day 1

---
+++ src/01/sonar_sweep.py
@@ -0,0 +1,21 @@
+def part1(data):
+    count = 0
+    current = data[0]
+    for depth in data:
+        count += (depth > current)
+        current = depth
+    return count
+
+
+def part2(data):
+    count = 0
+    for i in range(len(data)-3):
+        if sum(data[i+1:i+4]) > sum(data[i:i+3]):
+            count += 1
+    return count
+
+
+if __name__ == "__main__":
+    with open("input.txt") as f:
+        data = [int(depth) for depth in f.readlines()]
+    print(f"Part 1: {part1(data)}\nPart 2: {part2(data)}")
