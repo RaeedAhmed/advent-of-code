1381d89f6cfbfc48b63f7d366c87cbacfae27d2b
Raeed Ahmed <ruahmed7868@gmail.com>
2021-12-02 18:54:43 -0800

    No list slicing sliding window

--- src/01/sonar_sweep.py
+++ src/01/sonar_sweep.py
@@ -1,3 +1,9 @@
+from os import sep
+
+
+from pathlib import Path
+
+
 def part1(data):
     count = 0
     current = data[0]
@@ -7,14 +13,27 @@ def part1(data):
     return count
 
 
-def part2(data):
+def part2_old(data):
     count = 0
     for i in range(len(data)-3):
         count += (sum(data[i+1:i+4]) > sum(data[i:i+3]))
     return count
 
 
+def part2(data):
+    count = 0
+    window = [data.pop(0) for _ in range(3)]
+    last_sum = sum(window)
+    for depth in data:
+        window.append(depth)
+        window.pop(0)
+        current_sum = sum(window)
+        count += (sum(window) > last_sum)
+        last_sum = current_sum
+    return count
+
+
 if __name__ == "__main__":
-    with open("input.txt") as f:
+    with open(Path(__file__).absolute().parent / "input.txt") as f:
         data = [int(depth) for depth in f.readlines()]
-    print(f"Part 1: {part1(data)}\nPart 2: {part2(data)}")
+    print(part1(data), part2(data), sep="\n")
