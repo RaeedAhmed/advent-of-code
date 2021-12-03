b78e4c531b45832a81ea4a3edfe3a1344dcca53d
Raeed Ahmed <ruahmed7868@gmail.com>
2021-12-02 20:19:53 -0800

    Submit 2020 day 1

---
+++ src/2020/01/report_repair.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+
+
+def part1(data: set[int]) -> int:
+    for num in data:
+        if (complement := 2020-num) in data:
+            return num * complement
+
+
+def part2(data: set[int]) -> int:
+    for x in data:
+        for num in data:
+            if num != x and (complement := 2020-x-num) in data:
+                return x * num * complement
+
+
+def load_data(filename: str):
+    with open(Path(__file__).absolute().parent / filename) as f:
+        data = {int(depth) for depth in f.readlines()}
+    return data
+
+
+def test():
+    data = load_data("test.txt")
+    assert part1(data) == 514579
+    assert part2(data) == 241861950
+    print("\n***test passed***\n")
+
+
+def run():
+    data = load_data("input.txt")
+    return part1(data), part2(data)
+
+
+if __name__ == "__main__":
+    test()
+    p1, p2 = run()
+    print(p1, p2, sep="\n")
