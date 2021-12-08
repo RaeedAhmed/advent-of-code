18ff3a23898cc979e08fc1e522eec72970936e04
Raeed Ahmed <ruahmed7868@gmail.com>
2021-12-08 10:53:30 -0800

    2020 Day 10 complete

---
+++ src/2020/10/adapter_array.py
@@ -0,0 +1,43 @@
+import math
+from collections import deque
+from itertools import groupby
+
+from aoc.utils import load_data, profiler
+
+
+@profiler
+def part1(queue: deque[int]) -> int:
+    counter = {1: 0, 3: 0}
+    previous = 0
+    queue.append(max(queue) + 3)
+    while queue:
+        cursor = queue.popleft()
+        counter[cursor - previous] += 1
+        previous = cursor
+    return counter[1] * counter[3]
+
+
+@profiler
+def part2(queue: deque[int]) -> int:
+    queue.append(max(queue) + 3)
+    differences = []
+    previous = 0
+    while queue:
+        cursor = queue.popleft()
+        differences.append(cursor - previous)
+        previous = cursor
+    return math.prod(
+        (2 ** (len(m) - 1)) - (len(m) == 4)
+        for k, g in groupby(differences)
+        if k == 1 and len((m := list(g))) > 1
+    )
+
+
+def main():
+    data = sorted(list(map(int, load_data(2020, 10, test=False))))
+    print(part1(deque(data)))
+    print(part2(deque(data)))
+
+
+if __name__ == "__main__":
+    main()
