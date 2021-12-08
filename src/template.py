b07c449d248d301edf8644d4357b8d523539b580
Raeed Ahmed <ruahmed7868@gmail.com>
2021-12-07 20:10:47 -0800

    template for solutions

---
+++ src/template.py
@@ -0,0 +1,21 @@
+from aoc.utils import load_data, profiler
+
+
+@profiler
+def part1() -> None:
+    pass
+
+
+@profiler
+def part2() -> None:
+    pass
+
+
+def main():
+    data = load_data(2020, 9, test=True)
+    part1(data)
+    part2(data)
+
+
+if __name__ == "__main__":
+    main()
