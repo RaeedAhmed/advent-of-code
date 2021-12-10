823e7c43ac40e1d7bfbfeb087085bf9dbcb43ea2
Raeed Ahmed <ruahmed7868@gmail.com>
2021-12-09 19:29:32 -0800

    2021 Day 9 part 2 complete

--- src/2021/09/smoke_basin.py
+++ src/2021/09/smoke_basin.py
@@ -1,8 +1,14 @@
+import math
+from collections import deque, namedtuple
+
 from aoc.utils import load_data, profiler
+
+Grid = list[list[int]]
+Rules = namedtuple("Rules", ["xr", "yr", "value"])
 
 
 @profiler
-def part1(data: list[list[int]]) -> int:
+def part1(data: Grid) -> int:
     mx = len(data[0]) - 1
     my = len(data) - 1
     risk_levels = []
@@ -25,15 +31,45 @@ def part1(data: list[list[int]]) -> int:
     return sum(risk_levels)
 
 
+def walk(seed: tuple[int, int], grid: Grid, rules: Rules) -> int:
+    size = 0
+    dirs = ((0, 1), (0, -1), (1, 0), (-1, 0))
+    queue = deque([seed])
+    while queue:
+        y, x = queue.popleft()
+        if grid[y][x] != -1:
+            size += 1
+            grid[y][x] = -1
+            for dy, dx in dirs:
+                nx = x + dx
+                ny = y + dy
+                if (
+                    nx not in rules.xr
+                    and ny not in rules.yr
+                    and grid[ny][nx] not in rules.value
+                ):
+                    queue.append((ny, nx))
+    return size
+
+
 @profiler
-def part2() -> None:
-    pass
+def part2(data: Grid) -> int:
+    mx = len(data[0])
+    my = len(data)
+    sizes = []
+    rules = Rules((-1, len(data[0])), (-1, len(data)), (-1, 9))
+    for y in range(my):
+        for x in range(mx):
+            if data[y][x] not in [-1, 9]:
+                sizes.append(walk((y, x), data, rules))
+    return math.prod(sorted(sizes)[-3:])
 
 
 def main():
     data = load_data(2021, 9, test=False)
     data = [[int(x) for x in line] for line in data]
     print(part1(data))
+    print(part2(data))
 
 
 if __name__ == "__main__":
