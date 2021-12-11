8485e080d58321e25bf15bef9dccc6e872a69216
Raeed Ahmed <ruahmed7868@gmail.com>
2021-12-10 18:12:03 -0800

    Add find calling file

--- aoc/utils.py
+++ aoc/utils.py
@@ -1,17 +1,10 @@
-from pathlib import Path
+import inspect
+import sys
 from functools import wraps
+from pathlib import Path
 from time import perf_counter as timer
 
-
 datadir = "dev/advent-of-code/src"
-
-
-class InvalidEvent:
-    pass
-
-
-class ProblemNotFound:
-    pass
 
 
 def profiler(func):
@@ -23,19 +16,17 @@ def profiler(func):
         elapsed = toc - tic
         print(f"{func.__name__!r} took {elapsed:.6f} s")
         return value
+
     return wrapper_profiler
 
 
-def load_data(year: int, day: int, test=False) -> list[str]:
-    if 2015 <= year <= 2021 and 1 <= day <= 25:
-        try:
-            zero = "0"*(day < 10)
-            filename = "test.txt" if test else "input.txt"
-            path = Path.home() / datadir / f"{year}/{zero}{day}/{filename}"
-            with open(path) as f:
-                data = [line.strip() for line in f.readlines()]
-            return data
-        except FileNotFoundError:
-            raise ProblemNotFound
-    else:
-        raise InvalidEvent
+def load_data(test=False) -> list[str]:
+    try:
+        filename = "test.txt" if test else "input.txt"
+        source = inspect.getsourcefile(sys._getframe(1))
+        path = Path(source).parent / filename
+        with open(path) as f:
+            data = [line.strip() for line in f.readlines()]
+        return data
+    except FileNotFoundError as e:
+        raise e
